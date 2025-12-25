14cfb35ed35e3c96f8f75840dd0c69288b7e6903
Alekperova Exane <oalekperova.97@mail.ru>
2025-12-25 22:46:16 +0700

    test1

--- testovoe/find_task.py
+++ testovoe/find_task.py
@@ -5,3 +5,5 @@ from selene.api import *
 browser.open("https://avito-tech-internship-psi.vercel.app/issues")
 browser.element('//*[@id="root"]/div/div/div/div[1]/div[1]/div/div').type("Реализация")
 time.sleep(2)
+
+
