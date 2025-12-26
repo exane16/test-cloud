41ec385e1451596f884117ca6ff8763fa7fb850f
Alekperova Exane <oalekperova.97@mail.ru>
2025-12-26 18:09:31 +0700

    Test

--- testovoe/create_task.py
+++ testovoe/create_task.py
@@ -13,3 +13,7 @@ browser.element("/html/body/div[2]/div[3]/div/div[6]/div/div").click()
 browser.element('//*[@id=":r20:"]/li[2]').click()
 browser.element(by.text("Создать")).click()
 time.sleep(2)
+
+
+
+
